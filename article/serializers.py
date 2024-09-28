0368319bb6ce00a34c5a8cceb57437b0b7d7bf2a
Szymon Osypiuk <ciomek@tutanota.com>
2024-09-28 16:46:33 +0200

    Fix

--- article/serializers.py
+++ article/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 
 from article.models import Article
-from article.views import User
 from user.serializers import FriendSerializer
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class ArticleSerializer(serializers.ModelSerializer):
